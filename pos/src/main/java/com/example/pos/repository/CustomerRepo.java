89f1750f8f58d48fbd4ff7469ab1abe39c4746da
SitharaMadhusankha <166124345+SitharaMadhusankha@users.noreply.github.com>
2024-08-28 20:02:59 +0530

    use Jpa Repository  and EnableJpaRepositories annotation for CRUD operations

--- pos/src/main/java/com/example/pos/repository/CustomerRepo.java
+++ pos/src/main/java/com/example/pos/repository/CustomerRepo.java
@@ -1,4 +1,12 @@
 package com.example.pos.repository;
 
-public interface CustomerRepo {
+import com.example.pos.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface CustomerRepo extends JpaRepository <Customer,Integer>{
+
 }
--- pos/src/main/java/com/example/pos/service/impl/CustomerServiceIMPL.java
+++ pos/src/main/java/com/example/pos/service/impl/CustomerServiceIMPL.java
@@ -1,15 +1,31 @@
 package com.example.pos.service.impl;
 
 import com.example.pos.dto.CustomerDTO;
+import com.example.pos.entity.Customer;
+import com.example.pos.repository.CustomerRepo;
 import com.example.pos.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceIMPL implements CustomerService {
 
+    @Autowired
+    private CustomerRepo customerRepo;
 
     @Override
     public String saveCustomer(CustomerDTO customerDTO) {
+        Customer customer = new Customer(
+            customerDTO.getCustomerId(),
+            customerDTO.getCustomerName(),
+            customerDTO.getCustomerAddress(),
+            customerDTO.getContactNumber(),
+            customerDTO.getCustomerSalary(),
+            customerDTO.getNic(),
+            customerDTO.isActive()
+        );
+
+        customerRepo.save(customer);
         return null;
     }
 }
