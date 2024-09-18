aa2fd6c59a6a2abbbc8ec192b75cdc6d3263e206
SitharaMadhusankha <166124345+SitharaMadhusankha@users.noreply.github.com>
2024-09-18 22:52:15 +0530

    create class for model mapper configuration

--- pos/src/main/java/com/example/pos/service/impl/ItemServiceIMPL.java
+++ pos/src/main/java/com/example/pos/service/impl/ItemServiceIMPL.java
@@ -2,9 +2,10 @@ package com.example.pos.service.impl;
 
 import com.example.pos.dto.request.ItemSaveRequestDTO;
 import com.example.pos.entity.Item;
-import com.example.pos.entity.enums.MesuringUnitType;
 import com.example.pos.repository.ItemRepo;
 import com.example.pos.service.ItemService;
+import com.sun.jdi.request.DuplicateRequestException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,20 +14,19 @@ public class ItemServiceIMPL implements ItemService {
 
     @Autowired
     private ItemRepo itemRepo;
-
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
     public String saveItem(ItemSaveRequestDTO itemSaveRequestDTO) {
-        Item item = new Item(
-                1,
-                itemSaveRequestDTO.getItemName(),
-                itemSaveRequestDTO.getMeasuringUnitType(),
-                itemSaveRequestDTO.getBalanceQty(),
-                itemSaveRequestDTO.getSupplier(),
-                itemSaveRequestDTO.getSellingPrice(),
-                true
-        );
-        itemRepo.save(item);
-        return itemSaveRequestDTO.getItemName();
+        Item item=modelMapper.map(itemSaveRequestDTO,Item.class);
+        item.setActiveState(true);
+        if(!itemRepo.existsById(item.getItemId())){
+            itemRepo.save(item);
+            return item.getItemId()+"saved successfully";
+        }
+        throw new DuplicateRequestException("alredy Added");
     }
+
+
 }
